3caae37e61b3b098b245d85a512e7f71a080aaf7
M Arrayan <fighterfly9@gmail.com>
2025-03-09 14:51:56 +0700

    menambahkan implementasi dengan table-driven

--- Program.cs
+++ Program.cs
@@ -1,2 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class KodePos
+{
+    public int GetKodePos(string input)
+    {
+        string[] kelurahan = { "Batununggal", "Kujangsari", "Mengger", "Wates", "Cijaura", "Jatisari", "Margasari", "Sekejati", "Kebonwaru", "Maleer", "Samoja" };
+        int[] kodePos = { 40266, 40287, 40267, 40256, 40287, 40286, 40286, 40286, 40272, 40274, 40273 };
+
+        for (int i = 0; i < kelurahan.Length; i++)
+        {
+            if (kelurahan[i] == input)
+            {
+                return kodePos[i];
+            }
+        }
+        return -1;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        KodePos kodePos = new KodePos();
+        Console.Write("Masukkan nama kelurahan: ");
+        string input = Console.ReadLine();
+        int kode = kodePos.GetKodePos(input);
+
+        if (kode != -1)
+        {
+            Console.WriteLine("Kode pos dari kelurahan " + input + " adalah : " + kode);
+        }
+        else
+        {
+            Console.WriteLine("Kode Pos tidak ditemukan!");
+        }
+    }
+}
--- obj/Debug/net8.0/tpmodul4_103022300050.AssemblyInfo.cs
+++ obj/Debug/net8.0/tpmodul4_103022300050.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul4_103022300050")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04e4e4c2ea502f4752f6a73933b2ed612190f77e")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul4_103022300050")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul4_103022300050")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
